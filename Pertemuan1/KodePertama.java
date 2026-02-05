a1cace915cca591343e9011dc1636e32d0b2bb7b
ViolaineGunawan <violainegunawan06@gmail.com>
2026-02-05 15:19:33 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,12 @@
+/*
+Nama: Violaine Gunawan
+NIM: D1041241036
+Tanggal: Kamis, 5 Februari 2026
+*/
+package Pertemuan1;
+public class KodePertama {
+    public static void main(String[] args){
+        System.out.println("Halo, Pemrograman Berorientasi Object");
+        System.out.println("Ini adalah program java pertama saya");
+    }
+}
---
+++ Pertemuan1/KodePertemuan_Latihan.java
@@ -0,0 +1,14 @@
+/*
+Nama: Violaine Gunawan
+NIM: D1041241036
+Tanggal: Kamis, 5 Februari 2026
+*/
+package Pertemuan1;
+public class KodePertemuan_Latihan {
+    public static void main(String[] args){
+        System.out.println("Nama: Violaine Gunawan");
+        System.out.println("NIM: D1041241036");
+        System.out.println("Semester: 4");
+        System.out.println("Prodi: Informatika");        
+    }
+}
