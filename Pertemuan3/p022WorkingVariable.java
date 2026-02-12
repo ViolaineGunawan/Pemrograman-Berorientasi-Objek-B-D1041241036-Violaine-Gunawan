035addcaf6cec187157cf044bd3e053b22c9d00b
ViolaineGunawan <violainegunawan06@gmail.com>
2026-02-12 14:02:54 +0700

    Pertemuan 3

---
+++ Pertemuan3/p022WorkingVariable.java
@@ -0,0 +1,16 @@
+/*
+Nama: Violaine Gunawan
+NIM: D1041241036
+Tanggal: Kamis, 12 Februari 2026
+Program: Praktikum modul pertemuan 3, variabel
+*/
+package Pertemuan3;
+
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+        int angka = 10;
+        System.out.println("Nilai awal angka: " + angka);
+        angka = 25;
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
---
+++ Pertemuan3/p023PrimitiveData.java
@@ -0,0 +1,26 @@
+/*
+Nama: Violaine Gunawan
+NIM: D1041241036
+Tanggal: Kamis, 12 Februari 2026
+Program: Praktikum modul pertemuan 3, variabel primitif
+*/
+package Pertemuan3;
+
+public class p023PrimitiveData {
+    public static void main(String[] args) {
+        int umur = 20;
+        long populasi = 8000000000L;
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+
+        char nilaiHuruf = 'A';
+        boolean lulus = true;
+        
+        System.out.println("Umur           : " + umur);
+        System.out.println("Populasi       : " + populasi);
+        System.out.println("Tinggi Badan   : " + tinggi);
+        System.out.println("IPK            : " + ipk);
+        System.out.println("Nilai Huruf    : " + nilaiHuruf);
+        System.out.println("Status Lulus   : " + lulus);
+    }
+}
